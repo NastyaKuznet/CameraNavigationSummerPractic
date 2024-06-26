8117d6b12a5839e987023e50da2f48e1d981132a
Anastasiya Kuznetsova <115180166+NastyaKuznet@users.noreply.github.com>
2024-06-26 21:15:09 +0500

    generator start

---
+++ generator.py
@@ -0,0 +1,224 @@
+import random
+
+
+def generate_exit(x0, x1, y0, y1, size_cell_x, size_cell_y):
+    coord_axis = random.randint(0, 1)
+    wall = random.randint(0, 1)
+    if coord_axis == 0:
+        coord1 = random.randint(x0, x1 - 1) + size_cell_x / 2
+        coord2 = y0 if wall == 0 else y1
+    else:
+        coord2 = random.randint(y0, y1 - 1) + size_cell_y / 2
+        coord1 = x0 if wall == 0 else x1
+    return [coord1, coord2]
+
+
+def append_x_y(x, y, x_ap, y_ap):
+    x.append(x_ap)
+    y.append(y_ap)
+
+
+def generation_trajectory(x_exit, y_exit, x0_loc, x1_loc, y0_loc, y1_loc,
+                          size_cell_x, size_cell_y, count_max, early):
+    x = [x_exit]
+    y = [y_exit]
+    x_now, y_now = get_half_step(x, y, x_exit, y_exit, x0_loc, x1_loc,
+                                 y0_loc, y1_loc, size_cell_x, size_cell_y)
+    x1_exit = x_now
+    y1_exit = y_now
+    # 0 - лево, 1 - вверх, 2 - право, 3 - вниз
+
+    count = 0
+    state = None
+    while True:
+        direction = random.randint(0, 3)
+        match direction:
+            case 0:
+                step = x_now - size_cell_x
+                if step <= x0_loc:
+                    continue
+                x_now = step
+            case 1:
+                step = y_now + size_cell_y
+                if step >= y1_loc:
+                    continue
+                y_now = step
+            case 2:
+                step = x_now + size_cell_x
+                if step >= x1_loc:
+                    continue
+                x_now = step
+            case 3:
+                step = y_now - size_cell_y
+                if step <= y0_loc:
+                    continue
+                y_now = step
+        append_x_y(x, y, x_now, y_now)
+        count += 1
+        if x1_exit == x_now and y1_exit == y_now:
+            out = random.randint(0, 1)  # пойти на выход или нет?
+            if out == 0:
+                state = True
+                break
+
+        if count == count_max - early:
+            state = get_out(x, y, x_now, y_now, count, count_max, x_exit, y_exit, x1_exit, y1_exit,
+                            x0_loc, x1_loc, y0_loc, y1_loc, size_cell_x, size_cell_y)
+            break
+        if count == count_max:
+            break
+    return x, y, state
+
+
+def get_half_step(x, y, x_exit, y_exit, x0_loc, x1_loc, y0_loc, y1_loc, size_cell_x, size_cell_y):
+    if y_exit == y0_loc:  # вход снизу
+        x_now = x_exit
+        y_now = y_exit + size_cell_y / 2
+    elif y_exit == y1_loc:  # вход сверху
+        x_now = x_exit
+        y_now = y_exit - size_cell_y / 2
+    elif x_exit == x0_loc:  # вход слева
+        x_now = x_exit + size_cell_x / 2
+        y_now = y_exit
+    else:  # вход справа
+        x_now = x_exit - size_cell_x / 2
+        y_now = y_exit
+    append_x_y(x, y, x_now, y_now)
+    return x_now, y_now
+
+
+def get_out(x, y, x_now, y_now, count, count_max, x_exit, y_exit, x1_exit, y1_exit,
+            x0_loc, x1_loc, y0_loc, y1_loc, size_cell_x, size_cell_y):
+    while count <= count_max:
+        if y_exit == y0_loc:  # вход снизу
+            return get_out_down(x, y, x_now, y_now, count, count_max, x1_exit,
+                                y0_loc, size_cell_x, size_cell_y)
+
+        elif y_exit == y1_loc:  # вход сверху
+            return get_out_up(x, y, x_now, y_now, count, count_max, x1_exit,
+                              y1_loc, size_cell_x, size_cell_y)
+
+        elif x_exit == x0_loc:  # вход слева
+            return get_out_left(x, y, x_now, y_now, count, count_max, y1_exit,
+                                x0_loc, size_cell_x, size_cell_y)
+
+        else:  # вход справа
+            return get_out_right(x, y, x_now, y_now, count, count_max, y1_exit,
+                                 x1_loc, size_cell_x, size_cell_y)
+
+
+def get_out_down(x, y, x_now, y_now, count, count_max, x1_exit,
+                 y0_loc, size_cell_x, size_cell_y):
+    while True:  # идем до упора вниз
+        step = y_now - size_cell_y
+        if step <= y0_loc:
+            break
+        y_now = step
+        count += 1
+        append_x_y(x, y, x_now, y_now)
+        if count == count_max:
+            return False
+    return get_out_after_x(x, y, x_now, y_now, count, count_max, x1_exit, size_cell_x)
+
+
+def get_out_up(x, y, x_now, y_now, count, count_max, x1_exit,
+               y1_loc, size_cell_x, size_cell_y):
+    while True:  # идем до упора вверх
+        step = y_now + size_cell_y
+        if step >= y1_loc:
+            break
+        y_now = step
+        count += 1
+        append_x_y(x, y, x_now, y_now)
+        if count == count_max:
+            return False
+    return get_out_after_x(x, y, x_now, y_now, count, count_max, x1_exit, size_cell_x)
+
+
+def get_out_after_x(x, y, x_now, y_now, count, count_max, x1_exit,
+                    size_cell_x):
+    if x_now > x1_exit:  # если выход слева, то идем влево
+        while True:
+            step = x_now - size_cell_x
+            if step == x1_exit:
+                break
+            x_now = step
+            count += 1
+            append_x_y(x, y, x_now, y_now)
+            if count == count_max:
+                return False
+        return True
+    elif x_now < x1_exit:  # если выход справа, то идем вправо
+        while True:
+            step = x_now + size_cell_x
+            if step == x1_exit:
+                break
+            x_now = step
+            count += 1
+            append_x_y(x, y, x_now, y_now)
+            if count == count_max:
+                return False
+        return True
+    elif x_now == x1_exit:
+        return True
+
+
+def get_out_left(x, y, x_now, y_now, count, count_max, y1_exit,
+                 x0_loc, size_cell_x, size_cell_y):
+    while True:  # идем до упора влево
+        step = x_now - size_cell_x
+        if step <= x0_loc:
+            break
+        x_now = step
+        count += 1
+        append_x_y(x, y, x_now, y_now)
+        if count == count_max:
+            return False
+    return get_out_after_y(x, y, x_now, y_now, count, count_max, y1_exit, size_cell_y)
+
+
+def get_out_right(x, y, x_now, y_now, count, count_max, y1_exit,
+                  x1_loc, size_cell_x, size_cell_y):
+    while True:  # идем до упора вправо
+        step = x_now + size_cell_x
+        if step >= x1_loc:
+            break
+        x_now = step
+        count += 1
+        append_x_y(x, y, x_now, y_now)
+        if count == count_max:
+            return False
+    return get_out_after_y(x, y, x_now, y_now, count, count_max, y1_exit, size_cell_y)
+
+
+def get_out_after_y(x, y, x_now, y_now, count, count_max, y1_exit,
+                    size_cell_y):
+    if y_now > y1_exit:  # если выход снизу, то идем вниз
+        while True:
+            step = y_now - size_cell_y
+            if step == y1_exit:
+                break
+            y_now = step
+            count += 1
+            append_x_y(x, y, x_now, y_now)
+            if count == count_max:
+                return False
+        return True
+    elif y_now < y1_exit:  # если выход сверху, то идем вверх
+        while True:
+            step = y_now + size_cell_y
+            if step == y1_exit:
+                break
+            y_now = step
+            count += 1
+            append_x_y(x, y, x_now, y_now)
+            if count == count_max:
+                return False
+        return True
+    elif y_now == y1_exit:
+        return True
+
+
+if __name__ == '__main__':
+    count_max = random.randint(1, 200000)
+    early = random.randint(1, 100)
--- graph_system/graphsystem.py
+++ graph_system/graphsystem.py
@@ -1,31 +1,67 @@
 from plotly import graph_objs as go
-from setting_location import field, walls, cameras, selected_fields, exits
+import setting_location as sl
 import database.db as db
 
 
-def draw_location(fig):
-    # вывод поля
+def draw_field(fig, field):
     fig.add_shape(type="rect", x0=field[0][0], y0=field[0][1], x1=field[1][0],
                   y1=field[1][1], line=dict(color="black", width=2))
     fig.add_shape(type="rect", x0=field[0][0], y0=field[0][1], x1=field[1][0],
                   y1=field[1][1], fillcolor="lightblue",
                   line=dict(color="black", width=2), layer='below')
-    # вывод внутрених стен
+
+
+def draw_walls(fig, walls):
     for wall in walls:
         fig.add_trace(go.Scatter(x=wall[0], y=wall[1], mode='lines',
                                  showlegend=False, hoverinfo='none', line=dict(color='black', width=2)))
-    # ввывод камер
+
+
+def draw_cameras(fig, cameras):
     fig.add_trace(go.Scatter(x=cameras[0], y=cameras[1], mode='markers',
                              name='Camera', line=dict(color='blue')))
-    # ввывод зон камер
+
+
+def draw_selected_field(fig, selected_fields):
     for selected_field in selected_fields:
         fig.add_shape(type="rect", x0=selected_field[0][0], y0=selected_field[0][1],
                       x1=selected_field[1][0], y1=selected_field[1][1], fillcolor="#eec896",
                       line=dict(color="#e0993c", width=2), opacity=0.5)
-    # ввод указания где вход/выход
+
+
+def draw_exits(fig, exits):
     for ex in exits:
-        fig.add_annotation(x=ex[0], y=ex[1], text=ex[2],
+        fig.add_annotation(x=ex[0], y=ex[1], text="Вход/выход",
                            showarrow=True, arrowhead=7, arrowsize=1, arrowcolor='black')
+
+
+def draw_location(fig, field, walls=None, cameras=None, selected_fields=None, exits=None):
+    draw_field(fig, field)
+    if walls is not None:
+        draw_walls(fig, walls)
+    if cameras is not None:
+        draw_cameras(fig, cameras)
+    if selected_fields is not None:
+        draw_selected_field(fig, selected_fields)
+    if exits is not None:
+        draw_exits(fig, exits)
+
+
+def line_field(fig, width, height, x0_field, y0_field, x1_field, y1_field):
+    x0 = x0_field
+    while True:
+        x0 = x0 + width
+        if x0 >= x1_field:
+            break
+        fig.add_trace(go.Scatter(x=[x0, x0], y=[y0_field, y1_field], mode='lines',
+                                 showlegend=False, hoverinfo='none', line=dict(color='orange', width=2)))
+    y0 = y0_field
+    while True:
+        y0 = y0 + height
+        if y0 >= y1_field:
+            break
+        fig.add_trace(go.Scatter(x=[x0_field, x1_field], y=[y0, y0], mode='lines',
+                                 showlegend=False, hoverinfo='none', line=dict(color='orange', width=2)))
 
 
 def draw_trajectory(fig, id_person):
@@ -60,8 +96,8 @@ def draw_trajectory(fig, id_person):
 
 def draw():
     fig = go.Figure()
-    draw_location(fig)
-    draw_trajectory(fig, 3)
+    draw_location(fig, sl.field2)
+    line_field(fig, 1, 1, 0, 0, 50, 50)
     fig.show()
 
 
