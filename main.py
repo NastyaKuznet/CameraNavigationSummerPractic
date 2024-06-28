ee91714b14e12d09ec4439e1e271085d07973383
Tiques1 <tiques13@mail.ru>
2024-06-29 02:29:44 +0500

    Main structure

---
+++ main.py
@@ -0,0 +1,30 @@
+"""
+    Когда все пойдет по пизде, разбить стекло, достать этот скрипт
+
+    Этот скрипт запускает весь бэк, который работает в автомате
+    например ProcessManager
+
+    Класс Initializer создает все общие объекты
+"""
+
+from CameraNavigationSummerPractic.DBHelper import DBHelper
+from CameraNavigationSummerPractic.VideoHandling.ProcessManager import ProcessManager
+
+
+class Initializer:
+    def __init__(self):
+        self.db_helper = DBHelper(database='', user='', password='', host='')
+        self.process_manager = ProcessManager()
+
+
+class EntryPoint:
+    def __init__(self):
+        self.initializer = Initializer()
+
+    def main(self):
+        pass
+
+
+if __name__ == 'main':
+    entry_point = EntryPoint()
+    entry_point.main()
