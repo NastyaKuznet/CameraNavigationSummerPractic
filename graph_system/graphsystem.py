c45933c733c7e8a2a09667ceca4593fa07eaf52e
Anastasiya Kuznetsova <115180166+NastyaKuznet@users.noreply.github.com>
2024-06-29 19:26:06 +0500

    correct some medthods and add method which draw a lot of lines with moving points

--- graph_system/graphsystem.py
+++ graph_system/graphsystem.py
@@ -1,6 +1,7 @@
 from plotly import graph_objs as go
 import pandas as pd
 import plotly.express as px
+import plotly.colors as pcolors
 
 import setting_location as sl
 import database.db as db
@@ -8,25 +9,35 @@ import generator as gr
 
 
 def draw_field(fig, field):
-    fig.add_shape(type="rect", x0=field[0][0], y0=field[0][1], x1=field[1][0],
-                  y1=field[1][1], line=dict(color="black", width=2))
-    fig.add_shape(type="rect", x0=field[0][0], y0=field[0][1], x1=field[1][0],
-                  y1=field[1][1], fillcolor="lightblue",
+    """Отрисовывает на объекте fig прямоугольник. Field массив состоящий из двух массивов:
+    первый - левая нижняя точка x, y, второй - правая верхняя точка x, y."""
+    fig.add_shape(type="rect", x0=field[0][0], y0=field[0][1], x1=field[1][0], y1=field[1][1],
+                  line=dict(color="black", width=2))
+    fig.add_shape(type="rect", x0=field[0][0], y0=field[0][1], x1=field[1][0], y1=field[1][1],
+                  fillcolor="lightblue",
                   line=dict(color="black", width=2), layer='below')
 
 
 def draw_walls(fig, walls):
+    """Отрисовывает стены. Walls массив состоящий из массивов длиной 2,
+    где для i=0,n walls[i][0] = x, walls[i][1] = y."""
     for wall in walls:
         fig.add_trace(go.Scatter(x=wall[0], y=wall[1], mode='lines',
                                  showlegend=False, hoverinfo='none', line=dict(color='black', width=2)))
 
 
 def draw_cameras(fig, cameras):
+    """Отрисовывает камеры в виде синих точек. Cameras массив из двух подмассивов:
+    первый - все x, второй - все y."""
     fig.add_trace(go.Scatter(x=cameras[0], y=cameras[1], mode='markers',
                              name='Camera', line=dict(color='blue')))
 
 
 def draw_selected_field(fig, selected_fields):
+    """Отрисовывает полупрозрачные секции оранжевого цвета. Selected_fields массив,
+    состоящий из массивов, каждый из которых описывает одну секцию. Массив одной
+    секции состоит из двух массивов: первый - левая нижняя точка [x, y],
+    второй - правая верхняя [x, y]."""
     for selected_field in selected_fields:
         fig.add_shape(type="rect", x0=selected_field[0][0], y0=selected_field[0][1],
                       x1=selected_field[1][0], y1=selected_field[1][1], fillcolor="#eec896",
@@ -34,12 +45,15 @@ def draw_selected_field(fig, selected_fields):
 
 
 def draw_exits(fig, exits):
+    """Отрисовывает указатель с надписью Вход/выход. Exits массив состоящий из массивов,
+    каждый из которых содержит координаты x, y каждого входа."""
     for ex in exits:
         fig.add_annotation(x=ex[0], y=ex[1], text="Вход/выход",
                            showarrow=True, arrowhead=7, arrowsize=1, arrowcolor='black')
 
 
 def draw_location(fig, field, walls=None, cameras=None, selected_fields=None, exits=None):
+    """Отрисовывает поле, а также стены, камеры, выделенные поля, выходы, если они указаны."""
     draw_field(fig, field)
     if walls is not None:
         draw_walls(fig, walls)
@@ -52,6 +66,8 @@ def draw_location(fig, field, walls=None, cameras=None, selected_fields=None, ex
 
 
 def line_field(fig, width, height, x0_field, y0_field, x1_field, y1_field):
+    """Отрисовывает сетку по полю ограниченному x0_field, y0_field, x1_field, y1_field.
+    Ширина и высота ячейки сетки задается width, height."""
     x0 = x0_field
     while True:
         x0 = x0 + width
@@ -69,6 +85,9 @@ def line_field(fig, width, height, x0_field, y0_field, x1_field, y1_field):
 
 
 def draw_chessboard(fig, x0_field, x1_field, y0_field, y1_field, size_x, size_y):
+    """Отрисовывает полупрозрачную шахматную доску оранжевым и зеленым цветом по
+    полю ограниченному x0_field, x1_field, y0_field, y1_field, где ширина и длина
+    клеток задается size_x, size_y"""
     last_step_y = y0_field
     step_y = y0_field + size_y
     count = True
@@ -93,77 +112,25 @@ def draw_chessboard(fig, x0_field, x1_field, y0_field, y1_field, size_x, size_y)
         step_y += size_y
 
 
-def draw_trajectory(fig, id_person):  # переделать под новую бд
-    trajectory = db.get_trajectory(id_person)
-    cen = trajectory[0]
-    bli = [a for i in trajectory[1] for a in i]
-    ind = 0
-    i = 0
-    save = 0
-    x = []
-    y = []
-    while True:
-        if ind < len(bli) and cen[i][0] == bli[ind][4]:
-            x.append(bli[ind][0])
-            x.append(bli[ind][2])
-            y.append(bli[ind][1])
-            y.append(bli[ind][3])
-            save = bli[ind][5]
-            ind += 1
-        else:
-            if save != cen[i + 1][0]:
-                x.append(cen[i][1][0])
-                x.append(cen[i + 1][1][0])
-                y.append(cen[i][1][1])
-                y.append(cen[i + 1][1][1])
-            i += 1
-        if i == len(cen) - 1:
-            break
+def draw_trajectory(fig, x, y):
+    """Отрисовывается траетория по массивам x, y."""
+
     fig.add_trace(go.Scatter(x=x, y=y, mode='lines',
                              showlegend=True, line=dict(color='red', width=2)))
 
 
-def draw_test_location(x0, y0, x1, y1, size_x, size_y, width_window, height_window, count_max_start,
-                       count_max_end, early_start, early_end):
-    field = [[x0, y0], [x1, y1]]
-    ex = gr.generate_exit(x0, x1, y0, y1, size_x, size_y)
-    cam = gr.generate_cameras_all_cell(x0, x1, y0, y1, size_x, size_y)
-    fig = go.Figure()
-    draw_location(fig, field, exits=[ex], cameras=cam)
-    line_field(fig, size_x, size_y, x0, y0, x1, y1)
-    trajectory = gr.generation_trajectory(ex[0], ex[1], x0, x1, y0, y1, size_x, size_y,
-                                          count_max_start, count_max_end, early_start, early_end)
-    fig.add_trace(go.Scatter(x=trajectory[0], y=trajectory[1], mode='lines', text='trajectory',
-                             showlegend=True, line=dict(color='red', width=2)))
-    fig.update_layout(
-        xaxis_range=[x0, x1],
-        yaxis_range=[y0, y1],
-        xaxis_autorange=False,
-        yaxis_autorange=False,
-        width=width_window,
-        height=height_window,
-    )
-    fig.show()
-
-
-def draw_test_location_with_moving_point(x0, y0, x1, y1, size_x, size_y, width_window,
-                                         height_window, count_max_start,
-                                         count_max_end, early_start, early_end):
-    field = [[x0, y0], [x1, y1]]
-    ex = gr.generate_exit(x0, x1, y0, y1, size_x, size_y)
-    cam = gr.generate_cameras_all_cell(x0, x1, y0, y1, size_x, size_y)
-    trajectory = gr.generation_trajectory(ex[0], ex[1], x0, x1, y0, y1, size_x, size_y,
-                                          count_max_start, count_max_end, early_start, early_end)
+def draw_trajectory_with_point(fig, x, y):
+    """Отрисовывается траетория по массивам x, y с точкой показывающей передвижение."""
     fig = go.Figure(
-        data=[go.Scatter(x=trajectory[0], y=trajectory[1],
+        data=[go.Scatter(x=x, y=y,
                          mode="lines",
                          line=dict(width=2, color="green")),
-              go.Scatter(x=trajectory[0], y=trajectory[1],
+              go.Scatter(x=x, y=y,
                          mode="lines",
                          line=dict(width=2, color="green"))],
         layout=go.Layout(
-            xaxis=dict(range=[min(trajectory[0]), max(trajectory[0])], autorange=False, zeroline=False),
-            yaxis=dict(range=[min(trajectory[1]), max(trajectory[1])], autorange=False, zeroline=False),
+            xaxis=dict(range=[min(x), max(x)], autorange=False, zeroline=False),
+            yaxis=dict(range=[min(y), max(y)], autorange=False, zeroline=False),
             title_text="Траектория движения", hovermode="closest",
             updatemenus=[dict(type="buttons",
                               buttons=[dict(label="Play",
@@ -171,24 +138,54 @@ def draw_test_location_with_moving_point(x0, y0, x1, y1, size_x, size_y, width_w
                                             args=[None])])]),
         frames=[go.Frame(
             data=[go.Scatter(
-                x=[trajectory[0][k]],
-                y=[trajectory[1][k]],
+                x=[x[k]],
+                y=[y[k]],
                 mode="markers",
                 marker=dict(color="red", size=10))])
-            for k in range(len(trajectory[0]))]
+            for k in range(len(x))]
     )
-    draw_location(fig, field, exits=[ex], cameras=cam)
-    draw_chessboard(fig, x0, x1, y0, y1, size_x, size_y)
-    fig.update_layout(
-        xaxis_range=[x0, x1],
-        yaxis_range=[y0, y1],
-        xaxis_autorange=False,
-        yaxis_autorange=False,
-        width=width_window,
-        height=height_window,
-    )
-    fig.show()
-    return trajectory[2]
+
+
+def draw_a_lot_trajectory_with_point(fig, x_mas, y_mas,
+                                     colors_lines=pcolors.qualitative.Plotly,
+                                     colors_points=pcolors.qualitative.Set1):
+    """Отрисовывается траетории по массивам x_mas, y_mas с точками показывающими передвижение."""
+    points_traces = []
+    max_len = 0
+    for i in range(len(x_mas)):
+        fig.add_trace(go.Scatter(x=x_mas[i], y=y_mas[i],
+                                 mode="lines",
+                                 line=dict(width=2, color=colors_lines[i % len(colors_lines)])))
+    for i in range(len(x_mas)):
+        fig.add_trace(go.Scatter(x=x_mas[i], y=y_mas[i],
+                                 mode="lines",
+                                 line=dict(width=2, color=colors_lines[i % len(colors_lines)])))
+        points_traces.append(go.Scatter(x=[x_mas[i][0]], y=[y_mas[i][0]], mode="markers",
+                                        marker=dict(color=colors_points[i % len(colors_points)], size=10),
+                                        name=f'Point {i}'))
+        len_i = len(x_mas[i])
+        if len_i > max_len:
+            max_len = len_i
+    fig.update_layout(go.Layout(title_text="Траектория движения", hovermode="closest",
+                                updatemenus=[dict(type="buttons",
+                                                  buttons=[dict(label="Play",
+                                                                method="animate",
+                                                                args=[None])])]))
+    frames = []
+    for k in range(max_len):
+        frame_data = []
+        for i in range(len(x_mas)):
+            x_now = x_mas[i][-1] if k >= len(x_mas[i]) else x_mas[i][k]
+            y_now = y_mas[i][-1] if k >= len(x_mas[i]) else y_mas[i][k]
+            frame_data.append(go.Scatter(
+                x=[x_now],
+                y=[y_now],
+                mode="markers",
+                marker=dict(color=colors_points[i % len(colors_points)], size=10),
+                name=f'Point {i}'
+            ))
+        frames.append(go.Frame(data=frame_data))
+    fig.frames = frames
 
 
 def draw_heatmap_time_count_people(data):
@@ -221,6 +218,5 @@ if __name__ == "__main__":
         {'date': '2023-03-02', 'time': '11:00', 'visitors': 18},
         {'date': '2023-03-02', 'time': '12:00', 'visitors': 25},
     ]
-    #draw_heatmap_time_count_people(data0)
-    print(draw_test_location_with_moving_point(0, 0, 10, 10, 1, 1, 600, 600,
-                                         20, 40, 5, 10))
+    # draw_heatmap_time_count_people(data0)
+
