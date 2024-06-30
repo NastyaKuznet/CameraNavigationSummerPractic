866c9e8469ebb073c20e7bfe637552c897744830
Anastasiya Kuznetsova <115180166+NastyaKuznet@users.noreply.github.com>
2024-06-30 22:14:49 +0500

    will it work?

--- analyzerData.py
+++ analyzerData.py
@@ -2,13 +2,17 @@ from plotly import graph_objs as go
 
 import generator as gr
 import graphsystem as gs
+import database.db as db
+import database.config as cf
 
 
 class AnalyzerData:
     @staticmethod
     def compare_trajectories(x1, y1, times1, times2):
-        good = []
-        bad = []
+        good_x = []
+        good_y = []
+        bad_x = []
+        bad_y = []
         flag = False
         for i1 in range(len(times1)):
             for i2 in range(len(times2)):
@@ -16,10 +20,12 @@ class AnalyzerData:
                     flag = True
                     break
             if flag:
-                good.append((x1[i1], y1[i1]))
+                good_x.append(x1[i1])
+                good_y.append(y1[i1])
             else:
-                bad.append((x1[i1], y1[i1]))
-        return len(bad) == 0, good, bad
+                bad_x.append(x1[i1])
+                bad_y.append(y1[i1])
+        return len(bad_x) == 0, good_x, good_y, bad_x, bad_y
 
     @staticmethod
     def get_generate_traj(x0, y0, x1, y1, size_x, size_y, start_time,
@@ -51,4 +57,35 @@ class AnalyzerData:
             width=width_window,
             height=height_window,
         )
-        return fig.to_html()
+        fig.show()
+
+    @staticmethod
+    def start_demo():
+        x0_f = 0
+        y0_f = 0
+        x1_f = 30
+        y1_f = 30
+        s_x = 1
+        s_y = 1
+        width_w = 700
+        height_w = 700
+        starttime = "12:00"
+        endtime = "18:00"
+        x, y, state, times, ex = AnalyzerData.get_generate_traj(x0_f, y0_f, x1_f, y1_f, s_x, s_y, starttime, endtime)
+        # берем человека из бд
+        id_person = db.exec_query_first(f"""select id from {cf.schema_name}.person""",
+                                        "[INFO] Get first id_person")
+
+
+        # берутся его "фото" и отправляются на анализ лиц и результат сохраняется в бд с тем временем,
+        # что указано в times в таблице appearence
+
+
+        times2 = db.exec_query_all(f"""select data_time from {cf.schema_name}.appearence
+         where id_person == {id_person}""", "[INFO] Get time from appearence")
+        # по таблице appearence достаем time по id человека, которого мы взяли
+        state_compar, good_x, good_y, bad_x, bad_y = AnalyzerData.compare_trajectories(x, y, times, times2)
+
+        AnalyzerData.get_graph_traj_with_points(x0_f, y0_f, x1_f, y1_f, s_x, s_y, width_w, height_w,times,
+                                                ex, x, y, good_x, good_y)
+
