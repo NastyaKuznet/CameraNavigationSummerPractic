c0b577923a6209f2e44b967b4cbfec361e143202
Tiques1 <tiques13@mail.ru>
2024-07-02 09:12:06 +0500

    server faiss

--- VideoHandling/FAISS/server.py
+++ VideoHandling/FAISS/server.py
@@ -1,18 +1,20 @@
-import time
 import numpy as np
 import faiss
 import pickle
+from CameraNavigationSummerPractic.DBHelper import DBHelper
 
 
 class Vectors:
-    def __init__(self, npz):
-        data = np.load(npz)
-        embeddex_x = data['arr_0']  # 'embeddex_x'
-        Y = data['arr_1']  # Y
-
+    def __init__(self, npz=None):
         self.__name_to_vec = {}
-        for i in range(len(Y)):
-            self.__name_to_vec.setdefault(Y[i], []).append(embeddex_x[i])
+
+        if npz:
+            data = np.load(npz)
+            embeddex_x = data['arr_0']  # 'embeddex_x'
+            Y = data['arr_1']  # Y
+
+            for i in range(len(Y)):
+                self.__name_to_vec.setdefault(Y[i], []).append(embeddex_x[i])
 
     def get_vecs(self):
         vecs = []
@@ -24,7 +26,10 @@ class Vectors:
     def dump(self, path):
         pickle.dump(self.__name_to_vec, path)
 
-    def add(self, npz):
+    def add(self, vect, id_):
+        self.__name_to_vec.setdefault(id_, []).append(vect)
+
+    def add_npz(self, npz):
         data = np.load(npz)
         embeddex_x = data[0]
         Y = data[1]
@@ -62,20 +67,54 @@ class Faiss:
         return D, I
 
 
+class Server:
+    def __init__(self, db_helper):
+        self.db_helper = db_helper
+        self.faiss = Faiss()
+        self.vectors = Vectors()
+        self.__fill_vectors()
+
+        vecs = np.array(self.vectors.get_vecs())
+        self.faiss.train(vecs)
+        self.faiss.add(vecs)
+
+    def __fill_vectors(self):
+        self.db_helper.exec('select vector, id_person from photo')
+        pers = self.db_helper.fetch_one()
+        while pers:
+            self.vectors.add(pers[0], pers[1])
+
+    # Выбирает самого часто втречаемого, возвращает его id
+    def get_id_by_vec(self, vec):
+        d, i = self.faiss.search(np.array((vec,)), 5)
+        max_ = 0
+        for k in set(i[0]):
+            if i[0].count(k) > max_:
+                max_ = k
+        return max_
+
+
 # Обучение
 # Добавление векторов в индекс
 # Поиск
 if __name__ == '__main__':
-    vectors = Vectors("/home/tiques/PycharmProjects/CameraNavigation/CameraNavigationSummerPractic/VideoHandling/YOLO+MTCNN+FaceNet/faces_embeddings_done_4classes.npz")
-    vecs = vectors.get_vecs()
+    db_helper = DBHelper(database='big_brother', user='postgres', password='1111', host='localhost')
+    server = Server(db_helper)
 
-    faiss = Faiss()
-    faiss.train(np.array(vecs))
-    faiss.add(np.array(vecs))
+    vector666 = np.random.rand(512)
+    id_ = server.get_id_by_vec(vector666)
+    print(id_)
 
-    id_ = 10
-    print(vectors.vec_to_name(vecs[id_]), '\n')
-    vector = vecs[id_]
-    d, i = faiss.search(np.array((vector,)), 5)
-    for k in i[0]:
-        print(vectors.vec_to_name(vecs[k]))
+    # vectors = Vects("faces_embeddings_done_4classes.npz")
+    # vecs = vectors.get_vecs()
+    #
+    # faiss = Faiss()
+    # faiss.train(np.array(vecs))
+    # faiss.add(np.array(vecs))
+    #
+    # id_ = 5
+    # print(vectors.vec_to_name(vecs[id_]), '\n')
+    # vector = vecs[id_]
+    # d, i = faiss.search(np.array((vector,)), 5)
+    # for k in i[0]:
+    #     print(vectors.vec_to_name(vecs[k]))
