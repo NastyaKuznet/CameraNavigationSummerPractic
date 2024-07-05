9e1931fe2d0425d0fdfabbd9ba2e97ee32f34ef7
Anastasiya Kuznetsova <115180166+NastyaKuznet@users.noreply.github.com>
2024-07-05 21:12:24 +0500

    correct method for develop

--- analyzeData/analyzerData.py
+++ analyzeData/analyzerData.py
@@ -1,3 +1,4 @@
+import os
 import random
 
 from plotly import graph_objs as go
@@ -257,22 +258,11 @@ class AnalyzerData:
     @staticmethod
     def start_demo4(x0_f, y0_f, x1_f, y1_f, s_x, s_y, time_start, time_end, width_w, height_w):
         x, y, state, times, ex = AnalyzerData.get_generate_traj(x0_f, y0_f, x1_f, y1_f, s_x, s_y, time_start, time_end)
-        x2, y2, state2 = gr.Generator.generation_trajectory(ex[0], ex[1], x0_f, x1_f, y0_f, y1_f, s_x, s_y, len(times))
+
         good_x = []
         good_y = []
         bad_x = []
         bad_y = []
-        for i in range(len(x)):
-            flag = False
-            for j in range(len(x2)):
-                if x[i] == x2[j] and y[i] == y2[j]:
-                    good_x.append(x[i])
-                    good_y.append(y[i])
-                    flag = True
-                    break
-            if flag:
-                bad_x.append(x[i])
-                bad_y.append(y[i])
 
         field = [[x0_f, y0_f], [x1_f, y1_f]]
         # генерация камер
@@ -283,22 +273,35 @@ class AnalyzerData:
         times_a = []
 
         id_person = 1
-        path = [r"", r""]
+        path_directory = r""
+        file_paths = []
+        for root, _, files in os.walk(path_directory):
+            for file in files:
+                file_path = os.path.join(root, file)
+                file_paths.append(file_path)
+
         db_helper = DBHelper(database='big_brother', user='postgres', password='1111', host='localhost')
 
         '''generator = rf.RecognizeFromFile(db_helper)
         for i in range(len(x)):
-            id_p = generator.recognize(path[i % len(path)])
-            if id_p == id_person:
+            id_p = generator.recognize(file_paths[i % len(path)])
+            if id_p in id_person:
                 x_a.append(x[i])
                 y_a.append(y[i])
                 times_a.append(times[i])
+                good_x.append(x[i])
+                good_y.append(y[i])
+            else:
+                bad_x.append(x[i])
+                bad_y.append(y[i])
 
         fig = go.Figure()
-        gs.GraphSystem.draw_location(fig, field, exits=[ex], cameras=cam)  # локация, выход, камеры
+        gs.GraphSystem.draw_location(fig, field, exits=[ex])  # локация, выход, камеры
         gs.GraphSystem.draw_cameras_rect(fig, cam, 0.01)
         gs.GraphSystem.draw_chessboard(fig, x0_f, x1_f, y0_f, y1_f, s_x, s_y)  # разметка в виде шахматной доски
-        gs.GraphSystem.draw_a_lot_trajectory_with_point(fig, [x, x_a], [y, y_a], [times, times_a])  # траектории
+        gs.GraphSystem.draw_a_lot_trajectory_with_point(fig, [x, x_a], [y, y_a], [times, times_a],
+                                                        ["Сгенерированная траектория", "Проанализированная траектория"],
+                                                        ["Сгенерированное движение", "Проанализированное движение"])  # траектории
         fig.update_layout(
             xaxis_range=[x0_f, x1_f],
             yaxis_range=[y0_f, y1_f],
@@ -313,4 +316,4 @@ class AnalyzerData:
 
 
 if __name__ == "__main__":
-    AnalyzerData.start_demo2()
+    pass
